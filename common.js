8e568fc87103271c6c5a2fe84499da49917b8cc3
DmytroRoik <dmytroroik@gmail.com>
2017-12-19 17:13:48 +0200

    writtn localization

--- common.js
+++ common.js
@@ -1,31 +1,38 @@
+var currentLang;
+document.body.onload=function(){
+	var radios = document.querySelectorAll('input[type=radio]');
+	currentLang=localStorage.getItem('language')||"ua";
+
+	for(var el of radios){
+		if(currentLang==el.value){
+			applyLanguage(el.value)
+			el.checked=true;}
+		el.onchange=function (el) {
+			applyLanguage(this.value);
+		}
+	}
+}
+
 var applyLanguage = function (lang) {
-	alert('now language is: ' + lang);
-}
+	currentLang=lang;
 
+	var allLangEls=document.getElementsByClassName('lang');
+	for(var el of allLangEls){
+		el.classList.contains('visible')?el.classList.remove('visible') : "";
+	}
+	var langEls = document.getElementsByClassName('lang-' + currentLang);
+
+	for (var i=0; i<langEls.length; i++) {
+		var langEl = langEls[i];
+		langEl.classList.add('visible');
+	}
+}
 var getCurrentLanguage = function () {
-	var defaultLanguage = 'ua';
-	//
-  
-  return defaultLanguage;
+	return currentLang||"ua";
 }
 
-var currentLang = getCurrentLanguage();
-var langEls = document.getElementsByClassName('lang-' + currentLang);
-
-for (var i=0; i<langEls.length; i++) {
-	var langEl = langEls[i];
-	
-  // langEl.style.display = 'inline';
-  langEl.classList.add('visible');
-}
-
-
-
-
-
-// $<prefix> == DOMElement
 var $save = document.querySelector('html body button#save')
 
 $save.addEventListener('click', function(){
-	alert(1);
+	localStorage.setItem('language', getCurrentLanguage());
 });
